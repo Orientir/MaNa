4a75f4aa5a79d1981641d7d5e793bb86190a6f4c
Orientir <ya.orient@gmail.com>
2020-03-17 21:30:39 +0200

    Added templates

--- MaNa/urls.py
+++ MaNa/urls.py
@@ -17,12 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from ..goods.urls import urlpatterns as goods_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin"),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('', include(goods_urls, 'goods'), namespace="goods"),
+    path('', include('goods.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "MaNa"
