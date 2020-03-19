0bbe7378f27fdd280604337ad1743ec6c8620a56
Orientir <32954485+Orientir@users.noreply.github.com>
2020-03-19 18:55:25 +0200

    Update models.py

--- goods/models.py
+++ goods/models.py
@@ -1,5 +1,4 @@
 
-from djmoney.models.fields import MoneyField
 from django.utils.safestring import mark_safe
 from django.conf import settings
 from django.db import models
@@ -11,10 +10,10 @@ class Goods(models.Model):
     slug = models.SlugField(max_length=128, blank=True)
     title = models.CharField(verbose_name='название', max_length=200, blank=True)
     description = models.TextField(verbose_name='описание')
-
-    pln_price = MoneyField(verbose_name='злотые', help_text='цена в злотых', max_digits=14, decimal_places=2, default_currency='PLN')
-    uah_price = MoneyField(verbose_name='гривна', help_text='цена в гривне', max_digits=14, decimal_places=2, default_currency='UAH')
-    sale_price = MoneyField(verbose_name='цена', help_text='цена продажи', max_digits=14, decimal_places=2, default_currency='UAH')
+    pln_price = models.PositiveIntegerField(default=150, blank=True)
+    uah_price = models.PositiveIntegerField(default=150, blank=True)
+    sale_price = models.PositiveIntegerField(default=150, blank=True)
+    
     sales = models.BooleanField(verbose_name='продано', help_text='статус', default=False)
     height = models.PositiveIntegerField(default=150, blank=True)
     width = models.PositiveIntegerField(default=150, blank=True)
